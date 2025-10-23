5e064d065887557ef9425b0ba66204046ee8b3e8
FRC3574 <frcteam3574@gmail.com>
2025-10-22 19:45:30 -0700

    Intake Code!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -1,10 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Intake {
-    DcMotor intake;
+    private final Gamepad gamepad;
+    DcMotor intakeMotor;
 
-    public static void tick(boolean running) {
+    public Intake(HardwareMap hardwareMap, Gamepad gamepad) {
+        this.gamepad = gamepad;
+        intakeMotor = hardwareMap.get(DcMotor.class, "intake_motor");
+    }
 
+    public void tick() {
+        if(gamepad.dpad_down) {
+            intakeMotor.setPower(0.8);
+        }
+        else if(gamepad.dpad_up) {
+            intakeMotor.setPower(-0.8);
+        }
+        else {
+            intakeMotor.setPower(0);
+        }
     }
 }
